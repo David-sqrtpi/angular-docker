e6744a62ba6a76507cb8952c3823732703b391bd
David <david.stiven911@gmail.com>
2021-06-28 11:41:38 -0500

    Create component from debian

--- angular-docker/src/app/app.module.ts
+++ angular-docker/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlabamaComponent } from './components/alabama/alabama.component';
+import { SweetComponent } from './components/sweet/sweet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlabamaComponent
+    AlabamaComponent,
+    SweetComponent
   ],
   imports: [
     BrowserModule,
